3ddc85b01fe4b7770113339b1258793866ae551e
untitled <62313077+Beyzay@users.noreply.github.com>
2025-08-17 19:55:40 -0400

    Add a constructor to the base class

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // Set up classes
 
 // Create a base class
-class ProductProperties {}
+class ProductProperties {
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
